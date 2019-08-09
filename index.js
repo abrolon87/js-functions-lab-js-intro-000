6900bf0d5889cf4a40e1f082187b260ec4dcafdb
Amanda R <abrolon87@gmail.com>
2019-08-09 05:26:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,10 @@ function happyHolidaysTo(name) {
 function happyHolidayTo(holiday, name) {
     return `Happy ${holiday}, ${name}!`;
 }
+
+var days = '20';
+var holiday = 'Mother\'s Day'
+
+function holidayCountdown(holiday, days) {
+    return `It's ${days} days until ${holiday}!`; 
+}
