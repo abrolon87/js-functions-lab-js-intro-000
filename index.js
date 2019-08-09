ead261396b7a219ad3cdbf1f0ede495bf67c7240
Amanda R <abrolon87@gmail.com>
2019-08-09 05:07:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function happyHolidays() {
   return "Happy Holidays!";
 }
 //passed
-function happyHolidaysTo(name) {  
+function happyHolidaysTo(name) {
     var name = 'you';
     return `Happy holidays, ${name}!`;
 }
